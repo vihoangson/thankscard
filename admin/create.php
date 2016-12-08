2b938c69c696faa73fc053d0dee59c38c9e6ff9c
Vi Hoang Son <vihoangson@gmail.com>
2016-12-08 10:47:43 +0700

    change spec

--- admin/create.php
+++ admin/create.php
@@ -9,15 +9,22 @@ $db = new DB();
 $db->db_connect();
 
 if (isset($_POST["user_eid"])) {
-	$user_eid = $db->db_escape_string($_POST["user_eid"]);
-	$user_nick_name = $db->db_escape_string($_POST["user_nick_name"]);
-	$user_gwid = $db->db_escape_string($_POST["user_gwid"]);
-	$sql = "INSERT INTO user (user_eid, user_nick_name, user_reg_datetime, user_gwid) VALUES ('{$user_eid}', '{$user_nick_name}', NOW(), '{$user_gwid}')";
-	$db->db_query($sql);
+	try{
+		$user_eid = $db->db_escape_string($_POST["user_eid"]);
+		$extention_file = preg_match("/\.([a-z])+$/",$_FILES["user_image"]["name"],$match);
+		$file_name = $user_eid.$match[0];
+		move_uploaded_file($_FILES["user_image"]["tmp_name"],"../images/".$file_name);
+		$user_nick_name = $db->db_escape_string($_POST["user_nick_name"]);
+		$user_gwid = $db->db_escape_string($_POST["user_gwid"]);
+		$sql = "INSERT INTO user (user_eid, user_nick_name, user_reg_datetime, user_gwid,user_img) VALUES ('{$user_eid}', '{$user_nick_name}', NOW(), '{$user_gwid}','{$file_name}')";
+		$db->db_query($sql);
+	}catch (Exception $e){
+		echo 'Caught exception: ',  $e->getMessage(), "\n";
+		die;
+	}
 	header("Location: user.php");
-	
 }
-	
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -27,10 +34,11 @@ if (isset($_POST["user_eid"])) {
 
 <p></p>
 
-	<form name="create" action="create.php" method="post">
+	<form name="create" action="create.php" method="post" enctype="multipart/form-data">
 		<p><span>User EID:</span><input type="text" name="user_eid"></p>
 		<p><span>Nickname:</span><input type="text" name="user_nick_name"></p>
 		<p><span>GWID:</span><input type="text" name="user_gwid"></p>
+		<p><span>IMG:</span><input type="file" name="user_image"></p>
 		<p><input type="submit" value="Create"></p>
 	</form>
 </body>
